86b45821448aa2d1ad7b79ec1844bb57cd581661
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-26 01:14:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  num_array = [Math.sqrt(num)..Math.sqrt(2)]
+  if num_array.each |i|
+    num % i == 0 
+    return false
+  else  
+    return true
+  end    
+  
+  end  
+  
